484bf57a2d7f6599924ab5ec65d4d3556d6d31e4
Roadzik <102189021+Roadzik@users.noreply.github.com>
2023-08-15 22:21:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.querySelectorAll('.bt').forEach(h=>{
     h.style.width = '0'
     h.style.height = '0'
 })
+let bah =  document.querySelector('.yes').clientHeight
 faqs.forEach(e=>{
     e.addEventListener('click', f=>{
         if(e.lastElementChild != last && last != 0){
@@ -15,7 +16,7 @@ faqs.forEach(e=>{
             last.style.height = '0px'
             last.style.padding = '0'
             last.style.padding = '0'
-            document.querySelector('.yes').style.height = '270rem'
+            document.querySelector('.yes').style.height = bah + 'px'
         }
         if(e.style.height == size && size != 0) {
             e.style.height='4rem'
@@ -23,14 +24,14 @@ faqs.forEach(e=>{
             e.lastElementChild.style.width = '0px'
             e.lastElementChild.style.height = '0px'
             e.lastElementChild.style.padding = '0'
-            document.querySelector('.yes').style.height = '270rem'
+            document.querySelector('.yes').style.height = bah + 'px'
         }
         else{
             e.lastElementChild.style.display = 'block'
             e.lastElementChild.style.width = '100%'
             e.lastElementChild.style.height = '10rem'
             e.lastElementChild.style.padding = '2rem 2rem 1rem'
-            document.querySelector('.yes').style.height = '276rem'
+            document.querySelector('.yes').style.height = bah +200 + 'px'
             size = e.style.height = e.lastElementChild.lastElementChild.clientHeight+140+'px'
             last = e.lastElementChild
             lastel = e
